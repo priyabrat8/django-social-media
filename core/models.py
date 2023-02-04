f48e9b8f68356f7a8c8616cac12f4cb2ff9a75cf
priyabrat <priyabratswain888@gmail.com>
2023-02-04 20:52:07 +0530

    Add django app

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimage = models.ImageField(upload_to='profile_images',default='blank-profile-picture.jpg')
+    location = models.CharField(max_length=100,blank=True)
+
+    def __str__(self) -> str:
+        return self.user.username
---
+++ core/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+
+
+                user_model = User.objects.get(username=username)
+        else:
+            messages.info(request,"Password Not Matching")
+            return redirect('signup')
+        
+    
+    else:
+        return render(request,'signup.html')
