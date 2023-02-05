1df6327b07be282d05b5a4b4f4a00b125f9030df
priyabrat <priyabratswain888@gmail.com>
2023-02-05 22:10:03 +0530

    Craete User Profile

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path('signup', views.signup,name='signup'),
     path('signin', views.signin,name='signin'),
     path('logout', views.logout,name='logout'),
+    path('profile/<str:pk>', views.profile,name='profile'),
     path('settings', views.settings,name='settings'),
     path('upload', views.upload,name='upload'),
     path('like-post', views.like_post,name='like-post'),
+    path('follow', views.follow,name='follow'),
 
 ]
