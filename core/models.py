11a21fe869ef57187b40a812693c1510fe2c72e1
priyabrat <priyabratswain888@gmail.com>
2023-02-05 09:13:12 +0530

    Add post models

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 
 User = get_user_model()
 # Create your models here.
@@ -12,3 +14,15 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime)
+    no_of_likes = models.IntegerField(default=0)
+
+    
+    def __str__(self) -> str:
+        return self.user
--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 # Create your views here.
+
+@login_required(login_url='signin')
 def index(request):
     return render(request,'index.html')
 
@@ -23,12 +27,65 @@ def signup(request):
                 user = User.objects.create_user(username=username,email=email,password=password)
                 user.save()
 
+                user_login = auth.authenticate(username=username,password=password)
+                auth.login(request,user_login)
 
                 user_model = User.objects.get(username=username)
+                new_porfile = Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_porfile.save()
+                return redirect('settings')
         else:
             messages.info(request,"Password Not Matching")
             return redirect('signup')
-        
-    
     else:
         return render(request,'signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid Credentials')
+            return redirect('signin')
+    else:
+        return render(request,'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimage
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimage = image
+            user_profile.bio = bio
+            user_profile.location = location
+
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimage = image
+            user_profile.bio = bio
+            user_profile.location = location
+
+            user_profile.save()
+        return redirect(settings)   
+        
+    return render(request,'setting.html',{'user_profile':user_profile})
